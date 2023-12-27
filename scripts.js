f522b2f981c2df70e9874b8780dc1d982736aabb
Filipeelopess98 <filipee.lopess98@gmail.com>
2023-12-26 21:31:13 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,6 +7,14 @@ let numberScoreHuman = 0
 let numberScoreMachine = 0
 
 
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: "paper",
+    SCISSORS: 'scissors',
+}
+
+
+
 
 const playHuman = (humanchoice) => {
     playGame(humanchoice, playMachine())
@@ -14,7 +22,7 @@ const playHuman = (humanchoice) => {
 }
 
 const playMachine = () => {
-    const choices = ['rock', 'paper', 'scissors']
+    const choices = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS]
     const randomNumber = Math.floor(Math.random() * 3)
     return choices[randomNumber]
 }
@@ -26,10 +34,10 @@ const playGame = (human, machine) => {
     if (human === machine) {
         result.innerHTML = "Deu Empate 😬"
 
-    } else if (human === 'paper' && machine === 'rock' || human === 'rock' && machine === 'scissors' || human === 'scissors' && machine === 'paper') {
+    } else if (human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK || human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS || human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER) {
         numberScoreHuman++
         humanScore.innerHTML = numberScoreHuman
-        result.innerHTML = "Você Ganhou 🤩"
+        result.innerHTML = "Você Ganhou 😎"
 
     } else {
         numberScoreMachine++
